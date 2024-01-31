c0c524a62c50fbd6e5b7e4bc7510bd3269aa8ea7
Yarah Alghamdi <yalghamdi0188@stu.kau.edu.sa>
2024-01-31 11:28:30 +0300

    This is simple print statement

---
+++ src/main/java/com/mycompany/lab251/Lab251.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab251;
+
+/**
+ *
+ * @author yarahalghamdi
+ */
+public class Lab251 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello github ;)");
+    }
+}
